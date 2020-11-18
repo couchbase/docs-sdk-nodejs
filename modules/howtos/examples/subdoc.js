2f6972eb50ba8ecb3f089e30154d18aef6e21477
Eric Bishard <e.bishard@gmail.com>
2020-11-18 08:31:28 -0800

    Add Try/Catch to sub-doc pages Still need to analyze each code sample

--- modules/howtos/examples/subdoc.js
+++ modules/howtos/examples/subdoc.js
@@ -30,213 +30,300 @@ async function go() {
   });
 
   // tag::lookup-get[]
-  var result = await collection.lookupIn("customer123", [
-    couchbase.LookupInSpec.get("addresses.delivery.country"),
-  ]);
-  var country = result.content[0].value; //'United Kingdom'
+  try {
+    var result = await collection.lookupIn("customer123", [
+      couchbase.LookupInSpec.get("addresses.delivery.country"),
+    ]);
+    var country = result.content[0].value; //'United Kingdom'
+  } catch (e) {
+    // e == SubdocPathExistsError
+  }
   // end::lookup-get[]
 
   // tag::lookup-exists[]
-  var result = await collection.lookupIn("customer123", [
-    couchbase.LookupInSpec.exists("purchases.pending[-1]"),
-  ]);
-  console.log("Path exists? ", result.content[0].value);
+  try {
+    var result = await collection.lookupIn("customer123", [
+      couchbase.LookupInSpec.exists("purchases.pending[-1]"),
+    ]);
+    console.log("Path exists? ", result.content[0].value);
 
-  // Path exists? false
+    // Path exists? false
+  } catch (e) {
+    // e == SubdocPathExistsError
+  }
   // end::lookup-exists[]
 
   // tag::lookup-multi[]
-  var result = await collection.lookupIn("customer123", [
-    couchbase.LookupInSpec.get("addresses.delivery.country"),
-    couchbase.LookupInSpec.exists("purchases.pending[-1]"),
-  ]);
+  try {
+    var result = await collection.lookupIn("customer123", [
+      couchbase.LookupInSpec.get("addresses.delivery.country"),
+      couchbase.LookupInSpec.exists("purchases.pending[-1]"),
+    ]);
 
-  console.log(result.content[0].value); // United Kingdom
-  console.log("Path exists?", result.content[1].value); // false
+    console.log(result.content[0].value); // United Kingdom
+    console.log("Path exists?", result.content[1].value); // false
+    // Success
+  } catch (e) {
+    // e == SubdocPathExistsError
+  }
   // end::lookup-multi[]
 
   // tag::mutate-upsert[]
-  await collection.mutateIn("customer123", [
-    couchbase.MutateInSpec.upsert("fax", "311-555-0151"),
-  ]);
+  try {
+    await collection.mutateIn("customer123", [
+      couchbase.MutateInSpec.upsert("fax", "311-555-0151"),
+    ]);
+    // Success
+  } catch (e) {
+    // e == SubdocPathExistsError
+  }
   // end::mutate-upsert[]
 
   // tag::mutate-insert[]
-  await collection.mutateIn("customer123", [
-    couchbase.MutateInSpec.insert("purchases.complete", [42, true, "None"]),
-  ]);
-  // Success
-
   try {
     await collection.mutateIn("customer123", [
       couchbase.MutateInSpec.insert("purchases.complete", [42, true, "None"]),
     ]);
+    // Success
   } catch (e) {
     // e == SubdocPathExistsError
   }
   // end::mutate-insert[]
 
   // tag::mutate-multi[]
-  await collection.mutateIn("customer123", [
-    couchbase.MutateInSpec.remove("addresses.billing"),
-    couchbase.MutateInSpec.replace("email", "dougr96@hotmail.com"),
-  ]);
+  try {
+    await collection.mutateIn("customer123", [
+      couchbase.MutateInSpec.remove("addresses.billing"),
+      couchbase.MutateInSpec.replace("email", "dougr96@hotmail.com"),
+    ]);
+    // Success
+  } catch (e) {
+    // e == SubdocPathExistsError
+  }
   // end::mutate-multi[]
 
   // tag::mutate-arrayappend[]
-  await collection.mutateIn("customer123", [
-    couchbase.MutateInSpec.arrayAppend("purchases.complete", 777),
-  ]);
+  try {
+    await collection.mutateIn("customer123", [
+      couchbase.MutateInSpec.arrayAppend("purchases.complete", 777),
+    ]);
 
-  // purchases.complete is now [339, 976, 442, 666, 777]
+    // purchases.complete is now [339, 976, 442, 666, 777]
+  } catch (e) {
+    // e == SubdocPathExistsError
+  }
   // end::mutate-arrayappend[]
 
   // tag::mutate-arrayprepend[]
-  await collection.mutateIn("customer123", [
-    couchbase.MutateInSpec.arrayPrepend("purchases.abandoned", 18),
-  ]);
+  try {
+    await collection.mutateIn("customer123", [
+      couchbase.MutateInSpec.arrayPrepend("purchases.abandoned", 18),
+    ]);
 
-  // purchases.abandoned is now [18, 157, 49, 999]
+    // purchases.abandoned is now [18, 157, 49, 999]
+  } catch (e) {
+    // e == SubdocPathExistsError
+  }
   // end::mutate-arrayprepend[]
 
   // tag::mutate-tl-arrayappend[]
-  await collection.upsert("my_array", []);
-  await collection.mutateIn("my_array", [
-    couchbase.MutateInSpec.arrayAppend("", "some element"),
-  ]);
+  try {
+    await collection.upsert("my_array", []);
+    await collection.mutateIn("my_array", [
+      couchbase.MutateInSpec.arrayAppend("", "some element"),
+    ]);
 
-  // the document my_array is now ['some element']
+    // the document my_array is now ['some element']
+  } catch (e) {
+    // e == SubdocPathExistsError
+  }
   // end::mutate-tl-arrayappend[]
 
   // tag::mutate-tl-arrayappend-multi[]
-  await collection.mutateIn("my_array", [
-    couchbase.MutateInSpec.arrayAppend(false, "", "elem1", "elem2", "elem3"),
-  ]);
+  try {
+    await collection.mutateIn("my_array", [
+      couchbase.MutateInSpec.arrayAppend(false, "", "elem1", "elem2", "elem3"),
+    ]);
 
-  // the document my_array is now ['some_element', 'elem1', 'elem2', 'elem3']
+    // the document my_array is now ['some_element', 'elem1', 'elem2', 'elem3']
+  } catch (e) {
+    // e == SubdocPathExistsError
+  }
   // end::mutate-tl-arrayappend-multi[]
 
   // tag::mutate-tl-arrayappend-array[]
-  await collection.mutateIn("my_array", [
-    couchbase.MutateInSpec.arrayAppend("", ["elem1", "elem2", "elem3"]),
-  ]);
+  try {
+    await collection.mutateIn("my_array", [
+      couchbase.MutateInSpec.arrayAppend("", ["elem1", "elem2", "elem3"]),
+    ]);
 
-  // the document my_array is now ['some_element', ['elem1', 'elem2', 'elem3']]
+    // the document my_array is now ['some_element', ['elem1', 'elem2', 'elem3']]
+  } catch (e) {
+    // e == SubdocPathExistsError
+  }
   // end::mutate-tl-arrayappend-array[]
 
   // tag::mutate-tl-arrayappend-multibad[]
-  await collection.mutateIn("my_array", [
-    couchbase.MutateInSpec.arrayAppend("", "elem1"),
-    couchbase.MutateInSpec.arrayAppend("", "elem2"),
-    couchbase.MutateInSpec.arrayAppend("", "elem3"),
-  ]);
+  try {
+    await collection.mutateIn("my_array", [
+      couchbase.MutateInSpec.arrayAppend("", "elem1"),
+      couchbase.MutateInSpec.arrayAppend("", "elem2"),
+      couchbase.MutateInSpec.arrayAppend("", "elem3"),
+    ]);
+  } catch (e) {
+    // e == SubdocPathExistsError
+  }
   // end::mutate-tl-arrayappend-multibad[]
 
   // tag::mutate-createpaths[]
-  await collection.mutateIn("some_doc", [
-    couchbase.MutateInSpec.arrayAppend("some.array", "Hello", "World", {
-      createPath: true,
-    }),
-  ]);
+  try {
+    await collection.mutateIn("some_doc", [
+      couchbase.MutateInSpec.arrayAppend("some.array", "Hello", "World", {
+        createPath: true,
+      }),
+    ]);
+  } catch (e) {
+    // e == SubdocPathExistsError
+  }
   // end::mutate-createpaths[]
 
   // tag::mutate-arrayaddunique[]
-  await collection.mutateIn("customer123", [
-    couchbase.MutateInSpec.arrayAddUnique("purchases.complete", 95),
-  ]);
+  try {
+    await collection.mutateIn("customer123", [
+      couchbase.MutateInSpec.arrayAddUnique("purchases.complete", 95),
+    ]);
 
-  // => Success
+    // => Success
 
-  await collection.mutateIn("customer123", [
-    couchbase.MutateInSpec.arrayAddUnique("purchases.complete", 95),
-  ]);
+    await collection.mutateIn("customer123", [
+      couchbase.MutateInSpec.arrayAddUnique("purchases.complete", 95),
+    ]);
 
-  // => SubdocPathExists exception!
+    // => SubdocPathExists exception!
+  } catch (e) {
+    // e == SubdocPathExistsError
+  }
   // end::mutate-arrayaddunique-fail[]
 
   // tag::mutate-arrayinsert[]
-  await collection.mutateIn("array", [
-    couchbase.MutateInSpec.arrayInsert("[1]", "cruel"),
-  ]);
+  try {
+    await collection.mutateIn("array", [
+      couchbase.MutateInSpec.arrayInsert("[1]", "cruel"),
+    ]);
+  } catch (e) {
+    // e == SubdocPathExistsError
+  }
   // end::mutate-arrayinsert[]
 
   // tag::mutate-increment[]
-  var result = await collection.mutateIn("customer123", [
-    couchbase.MutateInSpec.increment("logins", 1),
-  ]);
+  try {
+    var result = await collection.mutateIn("customer123", [
+      couchbase.MutateInSpec.increment("logins", 1),
+    ]);
 
-  console.log(result.content[0]); // 1
+    console.log(result.content[0]); // 1
+  } catch (e) {
+    // e == SubdocPathExistsError
+  }
   // end::mutate-increment[]
 
   // tag::mutate-decrement[]
-  await bucket.upsert("player432", {
-    gold: 1000,
-  });
+  try {
+    await bucket.upsert("player432", {
+      gold: 1000,
+    });
 
-  var result = await collection.mutateIn("player432", [
-    couchbase.MutateInSpec.decrement("gold", 150),
-  ]);
+    var result = await collection.mutateIn("player432", [
+      couchbase.MutateInSpec.decrement("gold", 150),
+    ]);
   // => player 432 now has 850 gold remaining
+  } catch (e) {
+    // e == SubdocPathExistsError
+  }
   // end::mutate-decrement[]
 
   // tag::mutate-upsert-parents[]
-  await collection.mutateIn("customer123", [
-    couchbase.MutateInSpec.upsert(
-      "level_0.level_1.foo.bar.phone",
-      {
-        num: "311-555-0101",
-        ext: 16,
-      },
-      {
-        createPath: true,
-      }
-    ),
-  ]);
+  try {
+    await collection.mutateIn("customer123", [
+      couchbase.MutateInSpec.upsert(
+        "level_0.level_1.foo.bar.phone",
+        {
+          num: "311-555-0101",
+          ext: 16,
+        },
+        {
+          createPath: true,
+        }
+      ),
+    ]);
+  } catch (e) {
+    // e == SubdocPathExistsError
+  }
   // end::mutate-upsert-parents[]
 
   // tag::mutate-cas[]
-  await collection.mutateIn("customer123", [
-    couchbase.MutateInSpec.arrayAppend("purchases.complete", {
-      some_id: SOME_ID,
-    }),
-  ]);
+  try {
+    await collection.mutateIn("customer123", [
+      couchbase.MutateInSpec.arrayAppend("purchases.complete", {
+        some_id: SOME_ID,
+      }),
+    ]);
+  } catch (e) {
+    // e == SubdocPathExistsError
+  }
   // end::mutate-cas[]
 
   // tag::mutate-cas-noconflict[]
-  await collection.mutateIn("customer123", [
-    couchbase.MutateInSpec.arrayAppend("purchases.abandoned", {
-      some_other_id: SOME_OTHER_ID,
-    }),
-  ]);
+  try {
+    await collection.mutateIn("customer123", [
+      couchbase.MutateInSpec.arrayAppend("purchases.abandoned", {
+        some_other_id: SOME_OTHER_ID,
+      }),
+    ]);
+  } catch (e) {
+    // e == SubdocPathExistsError
+  }
   // end::mutate-cas-noconflict[]
 
   // tag::cas[]
-  await collection.mutateIn("customer123", [
-    couchbase.MutateInSpec.arrayAppend("addresses", "17 Olcott St"),
-  ], {
-    cas: SOME_CAS
-  });
+  try {
+    await collection.mutateIn("customer123", [
+      couchbase.MutateInSpec.arrayAppend("addresses", "17 Olcott St"),
+    ], {
+      cas: SOME_CAS
+    });
+  } catch (e) {
+    // e == SubdocPathExistsError
+  }
   // end::cas[]
 
   // tag::mutate-persistto[]
-  await collection.mutateIn(
-    "couchbase123",
-    [couchbase.MutateInSpec.insert("name", "mike")],
-    {
-      durabilityPersistTo: 1,
-      durabilityReplicateTo: 1,
-    }
-  );
+  try {
+    await collection.mutateIn(
+      "couchbase123",
+      [couchbase.MutateInSpec.insert("name", "mike")],
+      {
+        durabilityPersistTo: 1,
+        durabilityReplicateTo: 1,
+      }
+    );
+  } catch (e) {
+    // e == SubdocPathExistsError
+  }
   // end::mutate-persistto[]
 
   // tag::mutate-durability[]
-  await collection.mutateIn(
-    "couchbase123",
-    [couchbase.MutateInSpec.insert("name", "mike")],
-    {
-      durabilityLevel: couchbase.DurabilityLevel.Majority,
-    }
-  );
+  try {
+    await collection.mutateIn(
+      "couchbase123",
+      [couchbase.MutateInSpec.insert("name", "mike")],
+      {
+        durabilityLevel: couchbase.DurabilityLevel.Majority,
+      }
+    );
+  } catch (e) {
+    // e == SubdocPathExistsError
+  }
   // end::mutate-durability[]
 }
 go()
