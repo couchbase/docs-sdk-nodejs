64116d0411e9f18c33db694e01a9b3a5f41e4229
Hakim Cassimally <hakim.cassimally@couchbase.com>
2021-02-11 15:33:26 +0000

    DOC-7628 fix analytics.js example
     * fix some API errors, so that it runs to completion
     * comment requisite setup steps
     * improve the output
     * exit at the end of the script

--- modules/howtos/examples/analytics.js
+++ modules/howtos/examples/analytics.js
@@ -1,5 +1,11 @@
 "use strict";
 
+// NB: To run this file you will need:
+//    1) the travel-sample sample bucket loaded.
+//    2) an analytics node in the cluster
+//    3) a Dataset created in Analytics:
+//          CREATE DATASET airports ON `travel-sample` where `type` = 'airport';
+
 const couchbase = require("couchbase");
 
 async function go() {
@@ -13,41 +19,53 @@ async function go() {
 
   // tag::basic-query[]
   var result = await cluster.analyticsQuery('SELECT "hello" AS greeting');
-
   result.rows.forEach((row) => {
-    console.log(row.greeting);
+    console.log(row);
   });
   // end::basic-query[]
+  console.log();
+
+  function logResult (result) {
+    result.rows.forEach((row) => {
+      console.log(row);
+    });
+    console.log();
+  }
 
   // tag::simple-query[]
   var result = await cluster.analyticsQuery(
-    'SELECT airportname, country FROM airports WHERE country="France"'
+    'SELECT airportname, country FROM airports WHERE country="France" LIMIT 3'
   );
   // end::simple-query[]
+  logResult(result);
 
   // tag::posparam-query[]
   var result = await cluster.analyticsQuery(
-    "SELECT airportname, country FROM airports WHERE country = ?",
-    ["France"]
+    "SELECT airportname, country FROM airports WHERE country = ? LIMIT 3",
+    { parameters: ["France"] }
   );
   // end::posparam-query[]
+  logResult(result);
 
   // tag::namedparam-query[]
   var result = await cluster.analyticsQuery(
-    "SELECT airportname, country FROM airports WHERE country = $country",
-    { country: "France" }
+    "SELECT airportname, country FROM airports WHERE country = $country LIMIT 3",
+   { parameters:
+      { country: "France" } }
   );
   // end::namedparam-query[]
+  logResult(result);
 
   // tag::priority-query[]
   var result = await cluster.analyticsQuery(
-    'SELECT airportname, country FROM airports WHERE country="France"',
+    'SELECT airportname, country FROM airports WHERE country="France" LIMIT 3',
     {
       priority: true,
       timeout: 100, // seconds
     }
   );
   // end::priority-query[]
+  logResult(result);
 
   // tag::handle-rows[]
   var result = await cluster.analyticsQuery('SELECT "hello" AS greeting');
@@ -56,9 +74,10 @@ async function go() {
     console.log("Greeting: %s", row.greeting);
   });
   // end::handle-rows[]
+  console.log();
 
   // tag::handle-meta[]
-  var result = await cluster.queryAnalytics('SELECT "hello" AS greeting');
+  var result = await cluster.analyticsQuery('SELECT "hello" AS greeting');
 
   console.log("Elapsed time: %d", result.meta.metrics.elapsedTime);
   console.log("Execution time: %d", result.meta.metrics.executionTime);
@@ -68,4 +87,5 @@ async function go() {
 }
 go()
   .then((res) => console.log("DONE:", res))
-  .catch((err) => console.error("ERR:", err));
+  .catch((err) => console.error("ERR:", err))
+  .then(process.exit)
