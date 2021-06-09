04e8080724fe4a81e40c15bae4f1619433902bad
Hakim Cassimally <hakim.cassimally@couchbase.com>
2021-06-09 09:32:00 +0100

    DOC-7888 update RBAC for collection admin

--- modules/howtos/examples/collection-manager.js
+++ modules/howtos/examples/collection-manager.js
@@ -3,66 +3,92 @@
 const couchbase = require("couchbase");
 
 async function go() {
-  const cluster = new couchbase.Cluster("couchbase://localhost", {
+  const clusterAdm = new couchbase.Cluster("couchbase://localhost", {
     username: "Administrator",
     password: "password",
   });
 
-  var bucket = cluster.bucket("travel-sample");
+  // tag::scopeAdmin[]
 
-    // tag::create-collection-manager[]
-    const collectionMgr = bucket.collections();
-    // end::create-collection-manager[]
+  const userMgr = clusterAdm.users();
+  const bucketAdm = clusterAdm.bucket("travel-sample");
 
-    // tag::create-scope[]
-    try {
-      await collectionMgr.createScope("example-scope");
-    }
-    catch (e) {
-      if (e instanceof couchbase.ScopeExistsError) {
-        console.log("The scope already exists");
-      }
-    }
-    // end::create-scope[]
+  await userMgr.upsertUser({
+    username: "scope_admin",
+    password: "password",
+    displayName: "JS Manage Scopes [travel-sample:*]",
+    roles: [
+      { name: "scope_admin", bucket: "travel-sample" },
+      { name: "data_reader", bucket: "travel-sample" }
+    ]
+  })
+  // end::scopeAdmin[]
 
-    // tag::create-collection[]
-    try {
-      await collectionMgr.createCollection("example-collection", "example-scope");
-    }
-    catch (e) {
-      if (e instanceof couchbase.CollectionExistsError) {
-        console.log("The collection already exists");
-      }
-      // else if (e instanceof couchbase.ScopeNotFoundError) {
-      //   console.log("The scope does not exist");
-      // }
-    }
-    // end::create-collection[]
+  const cluster = new couchbase.Cluster("couchbase://localhost", {
+    username: "scope_admin",
+    password: "password",
+  });
 
-    // tag::drop-collection[]
-    try {
-      await collectionMgr.dropCollection("example-collection", "example-scope");
-    }
-    catch (e) {
-      // if (e instanceof couchbase.CollectionNotFoundError) {
-        console.log("The collection does not exist");
-      // }
-      // else if (e instanceof couchbase.ScopeNotFoundError) {
-      //   console.log("The scope does not exist");
-      // }
-    }
-    // end::drop-collection[]
+  // tag::create-collection-manager[]
+  const bucket = cluster.bucket("travel-sample");
+  const collectionMgr = bucket.collections();
+  // end::create-collection-manager[]
 
-    // tag::drop-scope[]
-    try {
-      await collectionMgr.dropScope("example-scope");
+  // tag::create-scope[]
+  try {
+    await collectionMgr.createScope("example-scope");
+  }
+  catch (e) {
+    if (e instanceof couchbase.ScopeExistsError) {
+      console.log("The scope already exists");
     }
-    catch (e) {
-      // if (e instanceof couchbase.ScopeNotFoundError) {
-        console.log("The scope does not exist");
-      // }
+    else {
+      throw e
     }
-    // end::drop-scope[]
+  }
+  // end::create-scope[]
+
+  // tag::create-collection[]
+  try {
+    await collectionMgr.createCollection("example-collection", "example-scope");
+  }
+  catch (e) {
+    if (e instanceof couchbase.CollectionExistsError) {
+      console.log("The collection already exists");
+    }
+    else {
+      throw e
+    }
+    // else if (e instanceof couchbase.ScopeNotFoundError) {
+    //   console.log("The scope does not exist");
+    // }
+  }
+  // end::create-collection[]
+
+  // tag::drop-collection[]
+  try {
+    await collectionMgr.dropCollection("example-collection", "example-scope");
+  }
+  catch (e) {
+    // if (e instanceof couchbase.CollectionNotFoundError) {
+      console.log("The collection does not exist");
+    // }
+    // else if (e instanceof couchbase.ScopeNotFoundError) {
+    //   console.log("The scope does not exist");
+    // }
+  }
+  // end::drop-collection[]
+
+  // tag::drop-scope[]
+  try {
+    await collectionMgr.dropScope("example-scope");
+  }
+  catch (e) {
+    // if (e instanceof couchbase.ScopeNotFoundError) {
+      console.log("The scope does not exist");
+    // }
+  }
+  // end::drop-scope[]
 }
 
 go()
