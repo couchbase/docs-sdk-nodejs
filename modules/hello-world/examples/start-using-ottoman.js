140e8bb810a6c7c324fa2fcbd04853cef1b0e060
Maria Shodunke <maria-robobug@users.noreply.github.com>
2021-09-29 17:25:12 +0100

    DOC-9087 - Add Ottoman Getting Started Page (#145)
    * Add Ottoman getting started page

---
+++ modules/hello-world/examples/start-using-ottoman.js
@@ -0,0 +1,85 @@
+'use strict'
+
+// tag::import[]
+const { Ottoman, Schema, SearchConsistency } = require('ottoman')
+const ottoman = new Ottoman()
+// end::import[]
+
+async function main() {
+  // tag::connect[]
+  var connection = await ottoman.connect({
+    connectionString: 'couchbase://localhost',
+    bucketName: 'travel-sample',
+    username: 'Administrator',
+    password: 'password',
+  })
+  // end::connect[]
+
+  // tag::alt-connect[]
+  connection = await ottoman.connect(
+    'couchbase://localhost/travel-sample@Administrator:password'
+  )
+  // end::alt-connect[]
+
+  // tag::schema[]
+  const airlineSchema = new Schema({
+    type: String,
+    id: Number,
+    name: String,
+    callsign: String,
+    iata: String,
+    icao: String,
+  })
+  // end::schema[]
+
+  // tag::model[]
+  const Airline = connection.model('airline', airlineSchema, {
+    scopeName: 'inventory',
+  })
+  // end::model[]
+
+  // tag::start[]
+  await connection.start()
+  // end::start[]
+
+  // tag::save[]
+  const airlineDoc = new Airline({
+    type: 'airline',
+    id: 8091,
+    callsign: 'CBS',
+    name: 'Couchbase Airways 1',
+    icao: 'CBICAO',
+    iata: 'CBIATA',
+  })
+
+  // Persist the airline document we just created in the database
+  await airlineDoc.save()
+  console.log(`Success: airline ${airlineDoc.name} added!\n`)
+  // end::save[]
+
+  // tag::alt-save[]
+  await Airline.create({
+    type: 'airline',
+    id: 8092,
+    callsign: 'CBS2',
+    name: 'Couchbase Airways 2',
+    icao: 'CBICAO2',
+    iata: 'CBIATA2',
+  })
+  // end::alt-save[]
+
+  // tag::find[]
+  const result = await Airline.find(
+    { callsign: 'CBS' },
+    { consistency: SearchConsistency.LOCAL }
+    // end::scan[]
+  )
+  console.log('Query Result: ', result.rows)
+  // end::find[]
+}
+
+// tag::run-main[]
+main()
+  .catch((error) => console.log('ERR:', error))
+  .finally(process.exit)
+// end::run-main[]
--- modules/test/test-hello-world.bats
+++ modules/test/test-hello-world.bats
@@ -18,3 +18,25 @@ GetResult {
   },
 EOF
 }
+
+@test "[hello-world] - start-using-ottoman.js" {
+    runExample $HELLO_WORLD_DIR start-using-ottoman.js
+    assert_success
+
+    # Check output for save[] snippet
+    assert_output --partial "Success: airline Couchbase Airways 1 added!"
+
+    # Check output for find[] snippet
+    assert_output --partial <<-EOF
+Query Result:  [
+  _Model {
+    callsign: 'CBS',
+    iata: 'CBIATA',
+    icao: 'CBICAO',
+    id: 8091,
+    name: 'Couchbase Airways 1',
+    type: 'airline'
+  }
+]
+EOF
+}
