e5a1a35135897e2a41257336181ae3250beca6e6
Maria Shodunke <maria-robobug@users.noreply.github.com>
2021-10-21 14:55:17 +0100

    Shorten Iterate comment as suggested.
    Co-authored-by: Simon Dew <39966290+simon-dew@users.noreply.github.com>

--- modules/hello-world/examples/n1ql-hello-world.js
+++ modules/hello-world/examples/n1ql-hello-world.js
@@ -10,8 +10,7 @@ async function go() {
   // Call the query() function on the cluster object and store the result.
   const result = await cluster.query('SELECT "Hello World" as greeting')
 
-  // The result object will include an array of rows found.
-  // We iterate over the rows to access our result data and print it to the terminal.
+  // Iterate over the rows to access result data and print to the terminal.
   result.rows.forEach((row) => {
     console.log(row)
   })
