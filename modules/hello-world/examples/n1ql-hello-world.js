5222dd801dcb76afb7f142bf954079c5792853f1
Maria Shodunke <maria-robobug@users.noreply.github.com>
2021-10-22 09:20:10 +0100

    Add some comments for Server re-org sample (#155)

--- modules/hello-world/examples/n1ql-hello-world.js
+++ modules/hello-world/examples/n1ql-hello-world.js
@@ -7,7 +7,10 @@ async function go() {
   })
 
   // tag::n1ql-query[]
+  // Call the query() function on the cluster object and store the result.
   const result = await cluster.query('SELECT "Hello World" as greeting')
+
+  // Iterate over the rows to access result data and print to the terminal.
   result.rows.forEach((row) => {
     console.log(row)
   })
