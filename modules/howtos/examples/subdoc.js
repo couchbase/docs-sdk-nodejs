582997496ed91433eff564a18f24cbcc1ba6854f
Maria Shodunke <mariashow2001@gmail.com>
2022-01-11 14:40:31 +0000

    DOC-9236 - Add missing example for mutateIn upsert/insert options

--- modules/howtos/examples/subdoc.js
+++ modules/howtos/examples/subdoc.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const couchbase = require('couchbase')
+const { StoreSemantics } = couchbase
 
 async function go() {
   const cluster = await couchbase.connect('couchbase://localhost', {
@@ -224,6 +225,29 @@ async function go() {
     ),
   ])
   // end::mutate-upsert-parents[]
+
+  console.log('mutate-store-semantics')
+  // tag::mutate-store-semantics[]
+  try {
+    // Mutate fields in a document that may or may not exist.
+    await collection.mutateIn(
+      'alice-123',
+      [
+        couchbase.MutateInSpec.insert('name', 'Alice'),
+        couchbase.MutateInSpec.upsert('email', 'alice@test.com'),
+      ],
+      {
+        storeSemantics: StoreSemantics.Upsert,
+      }
+    )
+  } catch (e) {
+    if (e instanceof couchbase.PathExistsError) {
+      console.log('Path already exists, not adding unique value')
+    } else {
+      throw e
+    }
+  }
+  // end::mutate-store-semantics[]
 
   console.log('mutate-cas')
   const SOME_ID = '100'
